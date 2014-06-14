c304401c1e699683379e6f4f1579a689fe070cfd
Amar Krishna <amar266@gmail.com>
2014-06-15 00:38:32 +0530

    Update site.pp
    updated fqdn

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,6 @@
 if "$::instance_type" == "horizon" {
 class { '::horizon':
-    fqdn                => "beta1.jiocloud.com",
+    fqdn                => "beta.jiocloud.com",
     secret_key          => "1212nn2jjds",
     keystone_url        => "https://identity-beta.jiocloud.com:443/v2.0",
     package_ensure      => "latest",
