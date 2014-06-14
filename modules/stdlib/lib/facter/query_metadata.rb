f53bd5d7c270b93135b95f2a950a16f7dcb791cb
Amar Krishna <amar266@gmail.com>
2014-06-15 02:03:47 +0530

    Update query_metadata.rb
    changed facter

--- modules/stdlib/lib/facter/query_metadata.rb
+++ modules/stdlib/lib/facter/query_metadata.rb
@@ -1,16 +1,17 @@
 # This script will query metadata
 # and create "facts" out of them for use within Puppet.
- 
+require "json"
 require 'facter'
- 
+
 # Get the instance-id and set it as a fact.
-cmd = sprintf('/usr/bin/wget -q -O - http://169.254.169.254/latest/meta-data/vm_type')
+cmd = sprintf('/usr/bin/wget -q -O - http://169.254.169.254/openstack/latest/meta_data.json')
 result = `#{cmd}`
+result = JSON.parse(result)
+instance_type = result["meta"]["vm_type"]
+memcached_add = result["meta"]["memcached"]
 Facter.add('instance_type') do
-    setcode { result }
+    setcode { instance_type }
 end
-cmd = sprintf('/usr/bin/wget -q -O - http://169.254.169.254/latest/meta-data/memcached')
-result = `#{cmd}`
 Facter.add('memcached_add') do
-    setcode { result }
+    setcode { memcached_add }
 end
